22fa8723e2ad174286b2ad49de85be9b4d181414
MelikhovAYu <MelikhovAYu@loodsen.ru>
2023-12-02 23:41:55 +0300

    Parsing text to contract

--- ms-tula-impl/src/main/java/domain/operations/ParseContractOperation.java
+++ ms-tula-impl/src/main/java/domain/operations/ParseContractOperation.java
@@ -1,33 +1,95 @@
 package domain.operations;
 
-import ch.qos.logback.core.util.ExecutorServiceUtil;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
-import net.sourceforge.tess4j.TesseractException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
 import org.springframework.stereotype.Service;
-import team.microchad.api.dto.LawsuitDto;
+import team.microchad.api.dto.*;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j
 @Service
 @NoArgsConstructor
 public class ParseContractOperation {
+    /**
+     * Plaintiff patterns
+     */
+    private static final Pattern INN_PATTERN = Pattern.compile("ИНН [0-9]{10}");
+    private static final Pattern OGRN_PATTERN = Pattern.compile("");
+    private static final Pattern KPP_PATTERN = Pattern.compile("КПП [0-9]{9}]");
+    private static final Pattern BIC_PATTERN = Pattern.compile("БИК [0-9]{9}");
 
     public LawsuitDto execute(InputStream pdfContract) throws IOException {
+        String pdfText = parsePdfToString(pdfContract);
+        log.info(pdfText);
+        LawsuitDto result = parseStringToLawsuit(pdfText);
+        return null;
+    }
+
+    private LawsuitDto parseStringToLawsuit(String pdfText) {
+        LawsuitDto result = new LawsuitDto();
+        result.setPlantiff(extractPlaintiff(pdfText));
+        result.setDefendant(extractDefendant(pdfText));
+        result.setArea(extractArea(pdfText));
+        result.setContract(extractContract(pdfText));
+        result.setPretension(extractPretension(pdfText));
+        result.setAgency(extractAgency(pdfText));
+        result.setRules(extractRules(pdfText));
+        return result;
+    }
+
+    private RulesDto extractRules(String text) {
+        RulesDto result = new RulesDto();
+        return result;
+    }
+
+    private GovermentAgencyDto extractAgency(String text) {
+        GovermentAgencyDto result = new GovermentAgencyDto();
+        return result;
+    }
+
+    private PretensionDto extractPretension(String text) {
+        PretensionDto result = new PretensionDto();
+        return result;
+    }
+
+    private AreaDto extractArea(String text) {
+        AreaDto result = new AreaDto();
+        return result;
+    }
+
+    private DefendantDto extractDefendant(String text) {
+        DefendantDto result = new DefendantDto();
+        return result;
+    }
+
+    private ContractDto extractContract(String text) {
+        ContractDto result = new ContractDto();
+        return result;
+    }
+
+    private PlaintiffDto extractPlaintiff(String text) {
+        PlaintiffDto result = new PlaintiffDto();
+        Matcher innMatcher = INN_PATTERN.matcher(text);
+        if (innMatcher.find()) result.setINN(innMatcher.group(1));
+        return result;
+    }
+
+    private String parsePdfToString(InputStream pdfContract) throws IOException {
         PDDocument document = PDDocument.load(pdfContract);
         StringBuilder result = new StringBuilder();
         PDFRenderer pdfRenderer = new PDFRenderer(document);
@@ -39,15 +101,14 @@ public class ParseContractOperation {
             futureList.add(service.submit(parser));
         }
         for (Future<String> future : futureList) {
-            while (!future.isDone());
+            while (!future.isDone()) ;
             try {
                 result.append(future.get());
             } catch (InterruptedException | ExecutionException e) {
                 throw new RuntimeException(e);
             }
         }
-        System.out.println(result);
-        return null;
+        return result.toString();
     }
 
     static class PdfToStringParser implements Callable<String> {
