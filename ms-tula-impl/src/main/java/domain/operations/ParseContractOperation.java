d09a943ca950056fb88d16b5fe9d641f23b1a3a7
MelikhovAYu <MelikhovAYu@loodsen.ru>
2023-12-02 23:06:22 +0300

    Adding multithreading parsing pdf to text

--- ms-tula-impl/src/main/java/domain/operations/ParseContractOperation.java
+++ ms-tula-impl/src/main/java/domain/operations/ParseContractOperation.java
@@ -1,5 +1,6 @@
 package domain.operations;
 
+import ch.qos.logback.core.util.ExecutorServiceUtil;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.sourceforge.tess4j.ITesseract;
@@ -8,7 +9,6 @@ import net.sourceforge.tess4j.TesseractException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import team.microchad.api.dto.LawsuitDto;
 
@@ -17,6 +17,10 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 @Slf4j
 @Service
@@ -27,24 +31,49 @@ public class ParseContractOperation {
         PDDocument document = PDDocument.load(pdfContract);
         StringBuilder result = new StringBuilder();
         PDFRenderer pdfRenderer = new PDFRenderer(document);
-        ITesseract tesseract = new Tesseract();
-        tesseract.setLanguage("rus");
+        ExecutorService service = Executors.newCachedThreadPool();
+        List<Future<String>> futureList = new ArrayList<>();
         for (int page = 0; page < document.getNumberOfPages(); page++) {
-            log.info("rendering image of page {}", page);
-            BufferedImage bufferedImage = pdfRenderer.renderImageWithDPI(page, 150, ImageType.GRAY);
-            log.info("end randering pf page {}", page);
-            File tempFile = File.createTempFile("tempfile_" + page, ".png");
-            ImageIO.write(bufferedImage, "png", tempFile);
-            log.info("start parsing image to text on page {}", page);
+            Callable<String> parser = new PdfToStringParser(pdfRenderer
+                    .renderImageWithDPI(page, 150, ImageType.GRAY), page);
+            futureList.add(service.submit(parser));
+        }
+        for (Future<String> future : futureList) {
+            while (!future.isDone());
             try {
-                result.append(tesseract.doOCR(tempFile));
-            } catch (TesseractException e) {
+                result.append(future.get());
+            } catch (InterruptedException | ExecutionException e) {
                 throw new RuntimeException(e);
             }
-            log.info("end parsing image on page {}", page);
-            tempFile.delete();
         }
         System.out.println(result);
         return null;
+    }
+
+    static class PdfToStringParser implements Callable<String> {
+
+        final BufferedImage bufferedImage;
+        final int page;
+
+        PdfToStringParser(BufferedImage bufferedImage, int page) {
+            this.bufferedImage = bufferedImage;
+            this.page = page;
+        }
+
+        @Override
+        public String call() {
+            File tempFile = null;
+            try {
+                ITesseract tesseract = new Tesseract();
+                tesseract.setLanguage("rus");
+                tempFile = File.createTempFile("tempfile_" + page, ".png");
+                ImageIO.write(bufferedImage, "png", tempFile);
+                return tesseract.doOCR(tempFile);
+            } catch (Exception e) {
+                return "ОШИБКА: Не удалось найти текст";
+            } finally {
+                tempFile.delete();
+            }
+        }
     }
 }
